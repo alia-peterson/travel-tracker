426d8be6d9f9001aee26fca925fead5657368116
Alia Peterson <alia@pnwebs.com>
2021-01-14 20:25:23 -0700

    Add functionality to find all traveler trips

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+// FILE IMPORTS
 import domUpdates from './domUpdates'
 import fetchApi from './fetchApi'
 
@@ -8,6 +9,7 @@ import Agent from './agent'
 import Trip from './trip'
 import Destination from './destination'
 
+// QUERY SELECTORS AND GLOBAL VARIABLES
 const travelerLoginButton = document.querySelector('#button-traveler')
 const agentLoginButton = document.querySelector('#button-agent')
 const logoffButton = document.querySelector('#button-logoff')
@@ -18,11 +20,17 @@ const travelerPassword = document.querySelector('#pass-traveler')
 
 let currentTraveler
 
+
+// EVENT LISTENERS
 travelerLoginButton.addEventListener('click', authenticateUser)
 agentLoginButton.addEventListener('click', authenticateUser)
 logoffButton.addEventListener('click', toggleScreen)
 
+// FETCH SERVER DATA
+const tripsResponse = fetchApi.getAllTrips()
+const destinationsResponse = fetchApi.getAllDestinations()
 
+// USER INFORMATION POPULATION
 function authenticateUser() {
   if (travelerUsername.value.includes('traveler') &&
       travelerPassword.value === 'travel2020') {
@@ -38,8 +46,19 @@ function authenticateUser() {
 function createTravelerProfile(traveler) {
   currentTraveler = new Traveler(traveler)
   domUpdates.populateTravelerInformation(currentTraveler)
+
+  Promise.all([tripsResponse])
+    .then(responses => {
+      findTravelerTrips(responses[0].trips)
+    })
 }
 
+function findTravelerTrips(allTrips) {
+  const travelerTrips = allTrips.filter(trip => trip.userID === currentTraveler.id)
+  console.log(travelerTrips);
+}
+
+// TOGGLE BETWEEN LOGIN AND DASHBOARD
 function toggleScreen() {
   dashboardView.classList.toggle('hidden')
   loginView.classList.toggle('hidden')
