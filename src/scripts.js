ac5cdb902248a3169439572358a1a1913073817d
Alia Peterson <alia@pnwebs.com>
2021-01-14 17:58:04 -0700

    Revise login screen

--- src/scripts.js
+++ src/scripts.js
@@ -7,3 +7,19 @@ import Traveler from './traveler'
 import Agent from './agent'
 import Trip from './trip'
 import Destination from './destination'
+
+const travelerLoginButton = document.querySelector('#button-traveler')
+const agentLoginButton = document.querySelector('#button-agent')
+const logoffButton = document.querySelector('#button-logoff')
+const dashboardView = document.querySelector('.dashboard')
+const loginView = document.querySelector('.login')
+
+travelerLoginButton.addEventListener('click', toggleScreen)
+agentLoginButton.addEventListener('click', toggleScreen)
+logoffButton.addEventListener('click', toggleScreen)
+
+function toggleScreen() {
+  dashboardView.classList.toggle('hidden')
+  loginView.classList.toggle('hidden')
+  logoffButton.classList.toggle('hidden')
+}
