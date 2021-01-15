597eca0b4df4879a621b00bcd41b313fa1d33b4e
Alia Peterson <alia@pnwebs.com>
2021-01-14 17:43:43 -0700

    Add login screen

---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+import domUpdates from './domUpdates'
+import fetchApi from './fetchApi'
+
+import './css/index.scss'
+
+import Traveler from './traveler'
+import Agent from './agent'
+import Trip from './trip'
+import Destination from './destination'
