fb14facd90df7f6d81929881986c2fcdf41490ac
Alia Peterson <alia@pnwebs.com>
2021-01-17 14:34:39 -0700

    Add test for calculating traveler spending

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -3,7 +3,9 @@ const expect = chai.expect
 
 import Traveler from '../src/traveler'
 import Trip from '../src/trip'
+import Destination from '../src/destination'
 import tripData from './trip-data'
+import destinationData from './destination-data'
 
 describe('Traveler Class', () => {
   let newTraveler
@@ -24,10 +26,17 @@ describe('Traveler Class', () => {
 
   it('should sort the user trips array by date', () => {
     newTraveler.sortTripsByDate()
-    expect(newTraveler.trips[0].date).to.equal('2020/10/04')
+    expect(newTraveler.trips[0].date).to.equal('2021/10/04')
+    expect(newTraveler.trips[1].date).to.equal('2021/05/22')
   })
 
   it('should calculate the total spending for a given year', () => {
-    
+    const yearBeforeLast = newTraveler.calculateSpending(destinationData, 2019)
+    const lastYearSpending = newTraveler.calculateSpending(destinationData, 2020)
+    const thisYearSpending = newTraveler.calculateSpending(destinationData, 2021)
+
+    expect(yearBeforeLast).to.equal('5,819.00')
+    expect(lastYearSpending).to.equal('6,567.00')
+    expect(thisYearSpending).to.equal('2,398.00')
   })
 })
