6f02de58341d894f2d8311dc9ae5ff442a85f79f
Alia Peterson <alia@pnwebs.com>
2021-01-17 12:41:47 -0700

    Add agent login dashboard styling

---
+++ test/trip-test.js
@@ -0,0 +1,22 @@
+import chai from 'chai'
+const expect = chai.expect
+
+import Traveler from '../src/traveler'
+import Trip from '../src/trip'
+import tripData from './trip-data'
+
+describe('Traveler Class', () => {
+  let newTraveler
+  const traveler1 = {id: 1, name: 'Oswin', type: 'relaxer'}
+
+  beforeEach(() => {
+
+  })
+
+  it('should create an instance of a trip', () => {
+
+  })
+
+  it('should sort the user trips array by date', () => {
+  })
+})
